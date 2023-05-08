54e8c4015d4d6080c1312eef84663ec966033321
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-05-08 18:54:02 -0300

    feat: fetching a single article

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -5,6 +5,11 @@ module Api
                 articles = Article.order('created_at DESC');
                 render json: {status: 'SUCCESS', message: 'Loaded Articles', data:articles}, stauts: :ok
             end
+
+            def show
+                article = Article.find(params[:id])
+                render json: {status: 'SUCCESS', message: 'Loaded Article', data:article}, stauts: :ok
+            end
         end
     end
 end
